db91c83dbde165e831932ee7f40b0543481cfc71
lkphuong <lekhphuong@gmail.com>
2021-11-27 22:13:43 +0700

    fix code and update vesion final

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticTenguCoffee = "tengu-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -17,12 +17,12 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticTenguCoffee).then(cache => {
       cache.addAll(assets);
     })
   );
 });
-/// very difficult
+
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
